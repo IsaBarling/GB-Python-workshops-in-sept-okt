e44e684422029b39972b1f47c045c995f9445213
«Mary <isabarling@gmail.com>
2022-11-09 13:42:11 +0300

    draft

---
+++ Seminar 10/parts of calculator/view_result.py
@@ -0,0 +1,11 @@
+def view(text, result):
+    print("{} {}".format(text, result))
+
+
+def input_data(text):
+    return input(text)
+
+
+def log_viev(filename):
+    with open(filename, "r") as data:
+        print(data.read())
